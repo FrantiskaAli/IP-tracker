e144753e03049c77b9298b3a47222f261e9533d9
FrantiskaAli <tiskarechk@gmail.com>
2023-05-06 21:36:05 +0100

    I managed to start the app, added map and search on without any style

---
+++ src/app/modules/header.js
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+      <header>
+        <h1>IP Address Tracker</h1>
+        <section>
+        <input type="text" placeholder="Search for any IP address or domain" />
+        <button></button>
+        </section>
+      </header>
+    )
+  }
